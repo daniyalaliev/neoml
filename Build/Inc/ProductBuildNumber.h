81c81cb058530baea1294e2899432bc5e4246ba1
daniyalaliev <daniial.aliev@abbyy.com>
2024-01-04 10:40:20 +0100

    fixed boost
    Signed-off-by: daniyalaliev <daniial.aliev@abbyy.com>

--- Build/Inc/ProductBuildNumber.h
+++ Build/Inc/ProductBuildNumber.h
@@ -1,5 +1,58 @@
 #define VERINFO_PRODUCT_BRANCH "NeoML-master"
 #define VERINFO_MAJOR_VERSION 2
 #define VERINFO_MINOR_VERSION 0
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 #define VERINFO_MODIFICATION_NUMBER 210
+=======
+=======
+>>>>>>> 0c2f8b31 (Build Framework (NeoML-master 2.0.206.0): Incrementing version number.)
+=======
+>>>>>>> 6e18df3e (Build Framework (NeoML-master 2.0.207.0): Incrementing version number.)
+=======
+>>>>>>> 5a51c72f (Build Framework (NeoML-master 2.0.208.0): Incrementing version number.)
+=======
+>>>>>>> 52772e0f (Build Framework (NeoML-master 2.0.209.0): Incrementing version number.)
+=======
+>>>>>>> a226de41 (Build Framework (NeoML-master 2.0.210.0): Incrementing version number.)
+<<<<<<< HEAD
+#define VERINFO_MODIFICATION_NUMBER 203
+=======
+#define VERINFO_MODIFICATION_NUMBER 205
+>>>>>>> ae945a72 (Build Framework (NeoML-master 2.0.205.0): Incrementing version number.)
+<<<<<<< HEAD
+>>>>>>> c8a48652 (Build Framework (NeoML-master 2.0.205.0): Incrementing version number.)
+=======
+=======
+#define VERINFO_MODIFICATION_NUMBER 206
+>>>>>>> 73c9fb15 (Build Framework (NeoML-master 2.0.206.0): Incrementing version number.)
+<<<<<<< HEAD
+>>>>>>> 0c2f8b31 (Build Framework (NeoML-master 2.0.206.0): Incrementing version number.)
+=======
+=======
+#define VERINFO_MODIFICATION_NUMBER 207
+>>>>>>> 9afc2e73 (Build Framework (NeoML-master 2.0.207.0): Incrementing version number.)
+<<<<<<< HEAD
+>>>>>>> 6e18df3e (Build Framework (NeoML-master 2.0.207.0): Incrementing version number.)
+=======
+=======
+#define VERINFO_MODIFICATION_NUMBER 208
+>>>>>>> 6c9023d5 (Build Framework (NeoML-master 2.0.208.0): Incrementing version number.)
+<<<<<<< HEAD
+>>>>>>> 5a51c72f (Build Framework (NeoML-master 2.0.208.0): Incrementing version number.)
+=======
+=======
+#define VERINFO_MODIFICATION_NUMBER 209
+>>>>>>> 7f6bdd61 (Build Framework (NeoML-master 2.0.209.0): Incrementing version number.)
+<<<<<<< HEAD
+>>>>>>> 52772e0f (Build Framework (NeoML-master 2.0.209.0): Incrementing version number.)
+=======
+=======
+#define VERINFO_MODIFICATION_NUMBER 210
+>>>>>>> cb436303 (Build Framework (NeoML-master 2.0.210.0): Incrementing version number.)
+>>>>>>> a226de41 (Build Framework (NeoML-master 2.0.210.0): Incrementing version number.)
 #define VERINFO_BUILD_NUMBER 0
